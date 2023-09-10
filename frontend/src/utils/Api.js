2b42a9fe8bf0bb3ff2382b18574855de610eb755
l1qwy <liqwy@yandex.ru>
2023-09-10 08:20:21 +0300

    edit_base_url_to_namedomain

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -97,7 +97,7 @@ class Api {
 }
 
 const api = new Api({
-  baseUrl: "http://localhost:3000/"
+  baseUrl: "https://api.liqwy.nomoredomainsicu.ru/"
 });
 
 export default api;
--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-const baseUrl = "http://localhost:3000";
+const baseUrl = "https://api.liqwy.nomoredomainsicu.ru";
 
 function getResponseData(res) {
   return res.ok
