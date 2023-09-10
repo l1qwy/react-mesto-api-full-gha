3217126809568af98173c1c438effb35049c276e
l1qwy <liqwy@yandex.ru>
2023-09-10 09:05:00 +0300

    router.use('/')

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -11,7 +11,7 @@ router.use('/signup', signupRouter);
 router.use(auth);
 router.use('/users', userRouter);
 router.use('/cards', cardsRouter);
-router.use('*', (req, res, next) => {
+router.use('/', (req, res, next) => {
   next(new NotFoundError('Страница не найдена'));
 });
 
